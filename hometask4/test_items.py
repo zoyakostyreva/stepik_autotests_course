e4ceb73101e0bdc35ec4e95e856e41895d232500
Zoya Kostyreva <zoya.kostyreva@gmail.com>
2020-07-08 09:53:59 +0500

    Add new fixed code for hometask 4 after review

--- hometask4/test_items.py
+++ hometask4/test_items.py
@@ -2,13 +2,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import pytest
 
-def main():
-    test_is_button_add_to_basket_present()
+@pytest.mark.test1
+def test_is_button_add_to_basket_present_on_page_coder_at_work(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
 
-def test_is_button_add_to_basket_present(browser):
+    time.sleep(5)
 
-    time.sleep(30)
+    # Ищем на странице кнопку добавления товара в корзину
+    try:
+        button = WebDriverWait(browser, 5).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, ".btn-add-to-basket")))
+        found = True
+
+    # Если кнопку не нашли (возникла ошибка), то выполняем этот кусок кода
+    except:
+        found = False
+
+    assert found == True, "Button is not found"
+
+@pytest.mark.test2
+def test_is_button_add_to_basket_present_on_page_hacking_exposed_wireless(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
+    browser.get(link)
+
+    time.sleep(5)
 
     # Ищем на странице кнопку добавления товара в корзину
     try:
@@ -23,4 +43,7 @@ def test_is_button_add_to_basket_present(browser):
     assert found == True, "Button is not found"
 
 if __name__ == '__main__':
-    main()
+    test_is_button_add_to_basket_present_on_page_coder_at_work()
+
+    test_is_button_add_to_basket_present_on_page_hacking_exposed_wireless()
+
